aa16f7c625bc0c14d81badf5c1b553792d1cda5d
E.S <extrin13@gmail.com>
2017-11-03 00:02:18 +0300

    fix: Fix book cover fetching

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,22 +1,26 @@
-import React from 'react'
-import Book from './Book'
+import React from 'react';
+import Book from './Book';
 
-function BookShelf (props) {
+function BookShelf(props) {
   return (
     <div className="bookshelf">
       <h2 className="bookshelf-title">{props.shelfTitle}</h2>
       <div className="bookshelf-books">
         <ol className="books-grid">
-          {props.books.map((book) => (
+          {props.books.map(book => (
             <li key={book.id}>
-            <Book
-              coverURL={book.previewLink}
-              bookShelf={book.shelf}
-              bookName={book.title}
-              bookAuthors={book.authors}
-            />
+              <Book
+                coverURL={book.imageLinks.thumbnail}
+                bookShelf={book.shelf}
+                bookName={book.title}
+                bookAuthors={book.authors}
+              />
+            </li>
           ))}
-  )
+        </ol>
+      </div>
+    </div>
+  );
 }
 
-export default BookShelf
+export default BookShelf;
