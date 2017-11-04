ec78961adfe9d762d689d8b00825bb6c1c9b6129
E.S <extrin13@gmail.com>
2017-11-04 19:05:03 +0300

    fix: Move API call to ListBooks Component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import * as BooksAPI from './BooksAPI';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import ListBooks from './ListBooks';
-import SearchBooks from './SearchBooks'
+import SearchBooks from './SearchBooks';
 import './App.css';
 
 class BooksApp extends React.Component {
-  state = {
-    books: [],
-  };
-
-  componentDidMount() {
-    BooksAPI.getAll().then(books => {
-      this.setState({ books });
-    });
-  }
-
   render() {
     return (
-    <Router>
-      <div className="app">     
-         <Route exact path="/" render={() => (
-          	<ListBooks books={this.state.books}/>
-         )}/>
-         <Route path="/search" render={() => (
-            <SearchBooks books={this.state.books}/>
-         )}/>	   
-      </div>
-</Router>
+      <Router>
+        <div className="app">
+          <Route exact path="/" render={() => <ListBooks />} />
+          <Route path="/search" render={() => <SearchBooks />} />
+        </div>
+      </Router>
     );
   }
 }
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,36 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
+import * as BooksAPI from './BooksAPI';
 import { Link } from 'react-router-dom';
 import BookShelf from './BookShelf';
 
-function ListBooks(props) {
-  return (
-    <div className="list-books">
-      <div className="list-books-title">
-        <h1>MyReads</h1>
-      </div>
-      <div className="list-books-content">
-        <div>
-          <BookShelf
-            shelfTitle="Currently Reading"
-            books={props.books.filter(
-              book => book.shelf === 'currentlyReading'
-            )}
-          />
-          <BookShelf
-            shelfTitle="Want to Read"
-            books={props.books.filter(book => book.shelf === 'wantToRead')}
-          />
-          <BookShelf
-            shelfTitle="Read"
-            books={props.books.filter(book => book.shelf === 'read')}
-          />
+class ListBooks extends Component {
+  state = {
+    books: []
+  };
+
+  componentDidMount() {
+    BooksAPI.getAll().then(books => {
+      this.setState({ books });
+    });
+  }
+
+  render() {
+    const books = this.state.books;
+
+    return (
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            <BookShelf
+              shelfTitle="Currently Reading"
+              books={books.filter(book => book.shelf === 'currentlyReading')}
+            />
+            <BookShelf
+              shelfTitle="Want to Read"
+              books={books.filter(book => book.shelf === 'wantToRead')}
+            />
+            <BookShelf
+              shelfTitle="Read"
+              books={books.filter(book => book.shelf === 'read')}
+            />
+          </div>
+        </div>
+        <div className="open-search">
+          <Link to="/search">Add a book</Link>
         </div>
       </div>
-      <div className="open-search">
-        <Link to="/search">Add a book</Link>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default ListBooks;
