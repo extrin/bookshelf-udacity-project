85789dda3f4b3962044ae7a69d8c5bde5990d2b1
E.S <extrin13@gmail.com>
2017-11-05 00:32:52 +0300

    feat: Add searching ability

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,19 +1,30 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-function SearchBar (props) {
-  
- return(
-   <div className="search-books-bar">
-     <Link
-     	className="close-search" 
-   		to="/"
-   	 >Close</Link>
-  	 <div className="search-books-input-wrapper">
-  	   <input type="text" placeholder="Search by title or author" />
-     </div>
-   </div>
-  );
+class SearchBar extends Component {
+  static propTypes = {
+    query: PropTypes.string,
+    updateQuery: PropTypes.func.isRequired
+  };
+
+  render() {
+    return (
+      <div className="search-books-bar">
+        <Link className="close-search" to="/">
+          Close
+        </Link>
+        <div className="search-books-input-wrapper">
+          <input
+            type="text"
+            placeholder="Search by title or author"
+            value={this.props.query}
+            onChange={event => this.props.updateQuery(event.target.value)}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default SearchBar;
--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,19 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import SearchBar from './SearchBar';
 import BookShelf from './BookShelf';
+import * as BooksAPI from './BooksAPI';
 
-function SearchBooks(props) {
-  return (
-    <div className="search-books">
-      <SearchBar />
-      <div className="search-books-results">
-        <BookShelf
-          shelfTitle="Search Results"
-          books={props.books.filter(book => book.shelf === 'none')}
-        />
+class SearchBooks extends Component {
+  state = {
+    books: [],
+    query: ''
+  };
+
+  updateQuery = query => {
+    const trimmedQuery = query.trim();
+    this.setState({ query: trimmedQuery });
+    BooksAPI.search(trimmedQuery, 20).then(books => {
+      this.setState({ books });
+    });
+  };
+
+  clearQuery = query => {
+    this.setState({ query: '' });
+  };
+
+  moveBook = (book, bookShelf) => {
+    const newBooks = this.state.books.map(
+      b => (b.id === book.id ? { ...b, shelf: bookShelf } : b)
+    );
+    this.setState(state => ({ books: newBooks }));
+    BooksAPI.update(book, bookShelf);
+  };
+
+  render() {
+    return (
+      <div className="search-books">
+        <SearchBar query={this.state.query} updateQuery={this.updateQuery} />
+        <div className="search-books-results">
+          <BookShelf
+            shelfTitle="Search Results"
+            books={this.state.books}
+            onBookMove={this.moveBook}
+          />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default SearchBooks;
